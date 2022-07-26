a7d2e4d8ed33bb681494c2c7a1d0f1c0e4fcf403
DESKTOP-VUT6VTF\nguye <nguyenvanlinh041298@gmail.com>
2022-07-26 13:58:04 +0700

    fixsomethiung

--- components/discoverinside/Leftside.js
+++ components/discoverinside/Leftside.js
@@ -1,8 +1,15 @@
 import Slider from "components/Slider";
-
+import { Recentlies } from "data/songData"
 export default function LeftSide() {
   return (
     <section>
+      {Recentlies.map((Recently)=> {
+        return (
+          <div>
+            <Image src={Recently.src}></Image>
+          </div>
+        )
+      })}
       <div>
         <p className="text-black">Recently Played</p>
       </div>
